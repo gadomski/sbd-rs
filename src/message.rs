c5fc068f12ce59696239a11fa709f2b6e36fa372
Pete Gadomski <pete.gadomski@gmail.com>
2015-08-28 13:12:31 -0500

    Return IoError if file DNE

--- src/message.rs
+++ src/message.rs
@@ -2,6 +2,7 @@
 //!
 //! This module provides the ability to read SBD messages from and write SBD messages to streams.
 
+use std::fs::File;
 use std::io::Read;
 use std::path::Path;
 
@@ -21,6 +22,7 @@ impl Message {
     /// let message = Message::from_path("data/0-mo.sbd").unwrap();
     /// ```
     pub fn from_path<P: AsRef<Path>>(path: P) -> Result<Message> {
+        let mut file = try!(File::open(path));
         Ok(Message)
     }
 
@@ -53,5 +55,10 @@ mod tests {
     fn from_read() {
         let mut file = File::open("data/0-mo.sbd").unwrap();
         Message::read_from(file).unwrap();
+    }
+
+    #[test]
+    fn from_path_that_doesnt_exist() {
+        assert!(Message::from_path("notafile.sbd").is_err());
     }
 }
