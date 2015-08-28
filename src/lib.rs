3c598756bc8778582d4e322123abeec4196426cd
Pete Gadomski <pete.gadomski@gmail.com>
2015-08-28 11:39:43 -0500

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,30 @@
+//! Receive, parse, store, and retrieve Iridium Short Burst Data (SBD) messages.
+//!
+//! Iridium is both a [satellite constellation](https://en.wikipedia.org/wiki/Iridium_satellite_constellation)
+//! and a [company](https://en.wikipedia.org/wiki/Iridium_Communications) that provides satellite
+//! communications. The Iridium network is used by phones, modems, and other communication devices.
+//!
+//! One mode of transmitting data over the Iridium network is via Short Burst Data (SBD) messages.
+//! These messages carry a payload of some small number of bytes, usually less than one thousand.
+//! Messages can be Mobile-Originated (MO), meaning that they are sent *from* an Iridium modem, or
+//! Mobile-Terminated (MT), meaning that the are sent *to* an Iridium modem. Mobile-originated
+//! messages are delivered either to an email address via MIME attachment, or directly to a given
+//! IP address and port via TCP; this second method is called DirectIP.
+//!
+//! This is a simple library for reading SBD messages from a stream, decoding their headers and
+//! data payloads, and writing them back to a stream.
+
+use std::result;
+
+#[derive(Debug)]
+pub enum Error {
+    IoError(std::io::Error),
+}
+
+impl From<std::io::Error> for Error {
+    fn from(err: std::io::Error) -> Error {
+        Error::IoError(err)
+    }
+}
+
+pub type Result<T> = result::Result<T, Error>;
