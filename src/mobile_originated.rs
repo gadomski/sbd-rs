f2112d56178751254ad313279e02e3e37120fb27
Pete Gadomski <pete.gadomski@gmail.com>
2015-08-28 16:04:59 -0500

    Open MO from path

--- src/mobile_originated.rs
+++ src/mobile_originated.rs
@@ -3,11 +3,28 @@
 //! SBD messages really can come in two forms, mobile-originated and mobile-terminated. This module
 //! provides functionality for the MO subset of messages.
 
+use std::path::Path;
+
 use Result;
 use message::Message;
 
 /// A mobile originated (MO) message;
 pub struct MobileOriginated;
+
+impl MobileOriginated {
+
+    /// Reads a mobile originated message from a `Path`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use sbd::mobile_originated::MobileOriginated;
+    /// MobileOriginated::from_path("data/0-mo.sbd");
+    /// ```
+    pub fn from_path<P: AsRef<Path>>(path: P) -> Result<MobileOriginated> {
+        Ok(try!(try!(Message::from_path(path)).into_mobile_originated()))
+    }
+}
 
 impl Message {
 
@@ -35,8 +52,13 @@ mod tests {
     use message::Message;
 
     #[test]
-    fn from_message() {
+    fn into_mobile_originated() {
         let message = Message::from_path("data/0-mo.sbd").unwrap();
         message.into_mobile_originated().unwrap();
+    }
+
+    #[test]
+    fn from_path() {
+        MobileOriginated::from_path("data/0-mo.sbd").unwrap();
     }
 }
