cf7462c83cd49969d5f31b26baa4467662539826
Pete Gadomski <pete.gadomski@gmail.com>
2015-08-30 13:22:23 -0500

    Report version

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,35 @@
 //! Command line utility for querying and working with Iridium SBD messages.
 
+extern crate docopt;
+extern crate rustc_serialize;
+
+use std::path::PathBuf;
+
+use docopt::Docopt;
+
+const USAGE: &'static str = "
+Iridium Short Burst Data (SBD) message utility.
+
+Usage:
+    sbd list <directory>
+    sbd (-h | --help)
+    sbd --version
+
+Options:
+    -h --help   Show this information
+    --version   Show version
+";
+
+#[derive(Debug, RustcDecodable)]
+struct Args {
+    arg_directory: PathBuf,
+}
+
+#[cfg_attr(test, allow(dead_code))]
 fn main() {
-    println!("Hello world");
+    let args: Args = Docopt::new(USAGE)
+        .and_then(|d| Ok(d.version(Some(env!("CARGO_PKG_VERSION").to_string()))))
+        .and_then(|d| d.decode())
+        .unwrap_or_else(|e| e.exit());
+    println!("{:?}", args);
 }
