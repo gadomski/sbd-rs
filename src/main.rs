8f1f96e627424a332aa7e10be258ec77da8fbacd
Pete Gadomski <pete.gadomski@gmail.com>
2015-10-24 14:58:40 -0500

    Add a read command to the binary
    This command reads in an sbd file then prints its payload to standard
    out. Error checking is very poor.

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,19 @@ extern crate docopt;
 extern crate rustc_serialize;
 extern crate sbd;
 
+use std::str;
+
 use docopt::Docopt;
 
 use sbd::filesystem::Storage;
+use sbd::message::Message;
 
 const USAGE: &'static str = "
 Iridium Short Burst Data (SBD) message utility.
 
 Usage:
     sbd list <directory>
+    sbd read <file>
     sbd (-h | --help)
     sbd --version
 
@@ -24,7 +28,9 @@ Options:
 #[derive(Debug, RustcDecodable)]
 struct Args {
     cmd_list: bool,
+    cmd_read: bool,
     arg_directory: String,
+    arg_file: String,
 }
 
 #[cfg_attr(test, allow(dead_code))]
@@ -36,6 +42,14 @@ fn main() {
     if args.cmd_list {
         for entry in &Storage::new(args.arg_directory) {
             println!("{}", entry.path_buf.to_str().unwrap());
+        }
+    }
+    if args.cmd_read {
+        match Message::from_path(args.arg_file) {
+            Ok(message) => {
+                println!("{}", str::from_utf8(message.payload_ref()).unwrap());
+            }
+            Err(err) => println!("ERROR: {:?}", err),
         }
     }
 }
