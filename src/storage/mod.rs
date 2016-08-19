9d00184db4e9465229725db00b4afe6d38e799f1
Pete Gadomski <pete.gadomski@gmail.com>
2016-08-19 10:13:18 -0600

    Add messages_from_imei method

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -38,4 +38,29 @@ pub trait Storage {
     /// assert_eq!(vec![message], messages);
     /// ```
     fn messages(&self) -> Result<Vec<Message>>;
+
+    /// Retrieves all messages for a given IMEI.
+    ///
+    /// The default implementation just filters the vector provided by `messages`, but some
+    /// implementors could be more intelligent.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use sbd::mo::Message;
+    /// # use sbd::storage::{Storage, MemoryStorage};
+    /// let message = Message::from_path("data/0-mo.sbd").unwrap();
+    /// let mut storage = MemoryStorage::new();
+    /// storage.store(message.clone());
+    /// let messages = storage.messages_from_imei("300234063904190").unwrap();
+    /// assert_eq!(vec![message], messages);
+    /// let messages = storage.messages_from_imei("300234063904191").unwrap();
+    /// assert!(messages.is_empty());
+    /// ```
+    fn messages_from_imei(&self, imei: &str) -> Result<Vec<Message>> {
+        self.messages().map(|mut v| {
+            v.retain(|m| m.imei() == imei);
+            v
+        })
+    }
 }
