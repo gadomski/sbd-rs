e8750b16d918ec0f55caf81f7876f4fbc40ee4ba
Pete Gadomski <pete.gadomski@gmail.com>
2018-09-11 08:15:18 -0600

    Reformat the module-level configuration

--- src/lib.rs
+++ src/lib.rs
@@ -40,10 +40,12 @@
 //! the `sbd` executable to inspect raw sbd files stores on a filesystem, interrogate sbd files on a
 //! filesystem, and start that forever-running server to receive Iridium SBD `DirectIP` messages.
 
-#![deny(missing_copy_implementations, missing_debug_implementations, missing_docs, trivial_casts,
-        trivial_numeric_casts, unsafe_code, unstable_features, unused_extern_crates,
-        unused_import_braces, unused_qualifications)]
-#![recursion_limit="128"]
+#![deny(
+    missing_copy_implementations, missing_debug_implementations, missing_docs, trivial_casts,
+    trivial_numeric_casts, unsafe_code, unstable_features, unused_extern_crates,
+    unused_import_braces, unused_qualifications
+)]
+#![recursion_limit = "128"]
 
 extern crate byteorder;
 extern crate chrono;
