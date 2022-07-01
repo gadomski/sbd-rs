2a575262a7da41243c0466f75119dd251f9b47c3
Guilherme Castelão <guilherme@castelao.net>
2022-06-30 20:54:08 -0600

    feat: MT Payload

--- src/mt/mod.rs
+++ src/mt/mod.rs
@@ -107,3 +107,28 @@ mod test_mt_header {
         );
     }
 }
+
+#[derive(Debug)]
+/// Mobile Terminated Payload
+///
+/// Note that length is a 2-bytes and valid range is 1-1890
+struct Payload {
+    payload: Vec<u8>,
+}
+
+impl Payload {
+    fn len(&self) -> usize {
+        self.payload.len()
+    }
+
+    fn write<W: std::io::Write>(&self, wtr: &mut W) -> Result<usize, Error> {
+        wtr.write_u8(0x42)?;
+        let n = self.payload.len();
+        wtr.write_u16::<BigEndian>(
+            n.try_into()
+                .expect("MT Payload's length was supposed to be u16"),
+        )?;
+        wtr.write(&self.payload)?;
+        Ok(3 + n)
+    }
+}
